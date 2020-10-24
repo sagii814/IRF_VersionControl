383ebf388b1d5ff3e82bad2a66a408f6ae1a9cae
sagii814 <s.agii814@gmail.com>
2020-10-24 11:15:31 +0200

    lists created

--- week07/week07/Form1.cs
+++ week07/week07/Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week07.Entities;
 
 namespace week07
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
         public Form1()
         {
             InitializeComponent();
