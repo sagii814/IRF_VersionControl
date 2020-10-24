e466868bf6a02a738480fd39f0805d07ddd05123
sagii814 <s.agii814@gmail.com>
2020-10-24 11:12:41 +0200

    classes created

---
+++ week07/week07/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrofChildren { get; set; }
+        public double P { get; set; }
+
+    }
+}
