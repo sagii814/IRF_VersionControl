df4f41a482396f8359c723252027160a610df118
sagii814 <s.agii814@gmail.com>
2020-11-14 14:16:15 +0100

    color picker button

--- week08/week08/Form1.Designer.cs
+++ week08/week08/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.ballButton = new System.Windows.Forms.Button();
             this.carButton = new System.Windows.Forms.Button();
+            this.colorButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -59,7 +60,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(194, 9);
+            this.label1.Location = new System.Drawing.Point(277, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(101, 20);
             this.label1.TabIndex = 1;
@@ -85,11 +86,22 @@
             this.carButton.UseVisualStyleBackColor = true;
             this.carButton.Click += new System.EventHandler(this.carButton_Click);
             // 
+            // colorButton
+            // 
+            this.colorButton.BackColor = System.Drawing.Color.Firebrick;
+            this.colorButton.Location = new System.Drawing.Point(191, 13);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(58, 33);
+            this.colorButton.TabIndex = 4;
+            this.colorButton.UseVisualStyleBackColor = false;
+            this.colorButton.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.colorButton);
             this.Controls.Add(this.carButton);
             this.Controls.Add(this.ballButton);
             this.Controls.Add(this.label1);
@@ -109,6 +121,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button ballButton;
         private System.Windows.Forms.Button carButton;
+        private System.Windows.Forms.Button colorButton;
     }
 }
 
