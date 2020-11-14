3479b4c1f38bf69dc697a66e1e21e67a816f8120
sagii814 <s.agii814@gmail.com>
2020-11-14 22:12:33 +0100

    present class (only rectangle)

---
+++ week08/week08/Entities/Present.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week08.Abstractions;
+
+namespace week08.Entities
+{
+    public class Present : Toy
+    {
+        public SolidBrush RibbonColor { get; private set; }
+        public SolidBrush BoxColor { get; private set; }
+
+        public Present(Color ribbon, Color box)
+        {
+            RibbonColor = new SolidBrush(ribbon);
+            BoxColor = new SolidBrush(box);
+
+        }
+        protected override void DrawImage(Graphics g)
+        {
+            g.FillRectangle(BoxColor, 0, 0, Width, Height);
+        }
+    }
+}
