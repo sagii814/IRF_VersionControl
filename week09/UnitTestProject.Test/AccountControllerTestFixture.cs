359bc679462a93a12218d2b241c209c7340c75a2
sagii814 <s.agii814@gmail.com>
2020-11-21 18:34:24 +0100

    create TestValidateEmail function

---
+++ week09/UnitTestProject.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTestProject.Test
+{
+    public class AccountControllerTestFixture
+    {
+
+        [Test ]
+        void TestValidateEmail(string email, bool expectedResult)
+        {
+
+        }
+    }
+}
