211d0e067054ade4480371260f65708646918108
sagii814 <s.agii814@gmail.com>
2020-11-21 21:21:47 +0100

    last bits before new (?)

---
+++ week09/UnitTestProject.Test/AccountControllerPasswordTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject.Test
+{
+    public class AccountControllerPasswordTest
+    {
+        [
+         Test,
+         TestCase("abcd", false),
+         TestCase("APPLE", false),
+         TestCase("cherry", false),
+         TestCase("Pear85", false),
+         TestCase("CoachPotato11", true)
+        ]
+        void TestPassword()
+        {
+
+        }
+
+    }
+}
--- week09/UnitTestProject.Test/AccountControllerTestFixture.cs
+++ week09/UnitTestProject.Test/AccountControllerTestFixture.cs
@@ -12,12 +12,13 @@ namespace UnitTestProject.Test
     {
 
         [
-         Test,
-         TestCase("abcd1234", false),
-         TestCase("irf@uni-corvinus", false),
-         TestCase("irf.uni-corvinus.hu", false),
-         TestCase("irf@uni-corvinus.hu", true)
-        ]
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+         ]
+
         void TestValidateEmail(string email, bool expectedResult)
         {
             //Arrange
