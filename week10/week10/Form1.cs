0bb4b1531e4a91619752dc020a4227271a6f9c67
sagii814 <s.agii814@gmail.com>
2020-11-28 21:47:23 +0100

    generation++

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -32,11 +32,21 @@ namespace week10
             //gc.AddPlayer();
             //gc.Start(true);
 
+            gc.GameOver += Gc_GameOver;
+
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
             }
             gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
     }
 }
